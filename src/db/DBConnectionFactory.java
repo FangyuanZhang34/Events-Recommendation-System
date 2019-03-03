1c94b7b90f89f2a691371ed4056145d3b6c86064
Fangyuan Zhang <zhan535@usc.edu>
2019-03-03 00:25:45 -0800

    add mongoDB part

--- src/db/DBConnectionFactory.java
+++ src/db/DBConnectionFactory.java
@@ -1,9 +1,10 @@
 package db;
 
+import db.mongodb.MongoDBConnection;
 import db.mysql.MySQLConnection;
 
 public class DBConnectionFactory {
-	private static final String DEFAULT_DB = "mysql";
+	private static final String DEFAULT_DB = "mongodb";
 
 	public static DBConnection getConnection(String db) {
 		switch (db) {
@@ -11,8 +12,7 @@ public class DBConnectionFactory {
 			return new MySQLConnection();
 //			return null;
 		case "mongodb":
-			// return new MongoDBConnection();
-			return null;
+			return new MongoDBConnection();
 		default:
 			throw new IllegalArgumentException("Invalid db: " + db);
 		}
